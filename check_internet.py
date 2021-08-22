4ccd05d0a66516eab5f50e55936e54680126a9a3
Rohit Sivakumar <sivakumar.rohit@gmail.com>
2021-08-22 11:40:50 +0530

    Added support to ping custom website by passing as cmdline argument. Eg: ./check_internet -s "www.xyz.com" or ./check_internet --site "xyz.com"

--- check_internet.py
+++ check_internet.py
@@ -1,15 +1,40 @@
 import requests
+import argparse
 
 
 def check_internet(url="https://8.8.8.8"):
+    internet_status = False
     timeout = 5
     if "https:" not in url:
         url = "https://" + url
     try:
         request = requests.get(url, timeout=timeout)
         print("Connected to the Internet")
+        internet_status = True
     except (requests.ConnectionError, requests.Timeout) as exception:
         print("No internet connection.")
+        internet_status = False
+
+    return internet_status
 
 
-check_internet("www.google.com")
+arg_descriptor = "Pass the URL to check internet connection.\n"
+arg_descriptor += "You can provide your custom site to ping as:\n ./check_internet.py -o \"www.google.com\""
+
+# Initialize the parser
+parser = argparse.ArgumentParser(description=arg_descriptor)
+
+# Add custom arguments
+parser.add_argument("-s", "--site", dest="site_to_ping")
+
+# Read arguments from command line
+args = parser.parse_args()
+site_to_ping = args.site_to_ping
+
+if site_to_ping is None:
+    print(arg_descriptor)
+    print("\nDefaulting to use :  www.google.com")
+    check_internet("www.google.com")
+else:
+    print("\nPinging %s" % site_to_ping)
+    check_internet(site_to_ping)
